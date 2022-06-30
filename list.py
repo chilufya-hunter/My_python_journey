1cbf47fbdf2197abe62f4cef8243088d1dab5966
chilufya-hunter <software.1995@mail.ru>
2022-06-30 21:12:33 +0200

    creating and manipulating lists

---
+++ list.py
@@ -0,0 +1,29 @@
+nums = [25,23,45,34,43]
+print(nums)
+print(nums[3])
+print(nums[2:])
+
+names=('john','jack','chilu','benny')
+print(names)
+
+values=(6.6,'john',5)
+#mil combines list
+mil=[nums,names]
+print(mil)
+
+nums.insert(5,100)
+print(nums)
+
+nums.append(806)
+print(nums)
+
+nums.remove(100)
+print(nums)
+nums.pop()
+print(nums)
+nums.extend([45,55,44,22])
+print(nums)
+
+min(nums)
+max(nums)
+print(max(nums))
