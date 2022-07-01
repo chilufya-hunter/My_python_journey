5b7f9074a3af8f992d16f0dbe133524204a3f758
chilufya-hunter <software.1995@mail.ru>
2022-07-01 20:29:12 +0200

    working with datatypes

---
+++ python_datatypes.py
@@ -0,0 +1,22 @@
+#(1) NONE
+#if a variable isnt assigned a value it is NONE
+#(2)NUMERIC 
+#we have muiltple types of numbers INT FLOAT COMPLEX BOOL
+#EXAMPLE FLOAT
+num = 2.5
+type(num)
+
+#INT 
+num = 2
+
+#complex
+num = 6+7j
+
+#converting a float into a INT
+a = 4.7
+b =int(a)
+print(b)
+z = 8
+
+c = complex(z,b)
+print(c)
