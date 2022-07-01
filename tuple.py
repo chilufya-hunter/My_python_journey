8e8842e9f7b5868b757d756631cd90a45184e5d1
chilufya-hunter <software.1995@mail.ru>
2022-07-01 20:04:17 +0200

    working with tuples

---
+++ tuple.py
@@ -0,0 +1,8 @@
+tup = (34,44,56,77,76,77)
+print(tup)
+#you can change value in tuples once it is create
+tuple=tup.count(77)
+print(tuple)
+#you can find the index of a tuple same way like an index of a list
+tuple=tup[3]
+print(tuple)
