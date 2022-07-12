58996c2f7babdc99809f785f6e0fa22ad6cfac0d
chilufya <chilufyamwape@gmail.com>
2022-07-12 19:28:45 +0200

    corrected errors on indetention

--- .idea/for_loop.py
+++ .idea/for_loop.py
@@ -8,5 +8,5 @@ if(x == 1):
     print("one")
 elif(x == 2):
     print("two") 
-elif(x==6)     
-print("nine")          
+elif(x==6):     
+    print("nine")          
--- .idea/list.py
+++ .idea/list.py
@@ -3,5 +3,5 @@ if(x == 1):
     print("one")
 elif(x == 2):
     print("two") 
-elif(x==6)     
-print("nine")          
+elif(x==6):     
+    print("nine")          
