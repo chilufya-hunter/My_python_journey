5ab21ff35411f747be49ca5c40dddf4d8d42a772
chilufya <chilufyamwape@gmail.com>
2022-07-12 19:23:55 +0200

    Update if_elif_else.py

--- .idea/if_elif_else.py
+++ .idea/if_elif_else.py
@@ -13,9 +13,5 @@ if(x == 1):
     print("one")
 elif(x == 2):
     print("two") 
-elif(x == 3):
-    print("three") 
-elif(x == 4):
-    print("four") 
-else:
-    print("wrong input")                     
+elif(x==6)     
+print("nine")          
