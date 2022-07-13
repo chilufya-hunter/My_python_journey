54410a80be79cb85fdd358871ed4212a1b6b6157
chilufya <chilufyamwape@gmail.com>
2022-07-13 17:43:48 +0200

    Update for_loop.py

--- .idea/for_loop.py
+++ .idea/for_loop.py
@@ -4,4 +4,9 @@ for i in range(1,12):
     if(i%5!=0):
      print(i)
 x = int(input("Enter any digit: "))
-         
+if(x == 1):
+    print("one")
+elif(x == 2):
+    print("two") 
+elif(x==6):     
+    print("nine")          
