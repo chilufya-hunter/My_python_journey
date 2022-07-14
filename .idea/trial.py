9d049a496b468f4b6691f4b7ed769ff24e75a57f
chilufya <chilufyamwape@gmail.com>
2022-07-14 14:43:59 +0200

    Update trial.py

--- .idea/trial.py
+++ .idea/trial.py
@@ -1,12 +1,15 @@
 x = int(input("Enter the value of X : "))
-if x>2:
+if x<202:
     print("Enter a larger value of x")
+    
 y = int(input("Enter the value of y : "))
-if y>2:
+if y<2:
     print("Enter a larger value of y")
+    
 z = int(input("Enter the value of z : "))
-if z>2:
-    print("Enter a larger value of x")
+if z<5:
+    print("Enter a larger value of z")
     print("The sum is: ")
+    
 summa =(x+y+z)
 print(summa)
